f19d2c943e81763e6b43b29cbb2ebf057941ac0d
Dre Johnson <dre@phreshr.com>
2020-08-29 19:32:22 +0800

    remove wrong package name

--- OpenWrt-R7800.sh
+++ OpenWrt-R7800.sh
@@ -10,9 +10,8 @@ cp .config.init .config
 cat >> .config <<EOF
 CONFIG_PACKAGE_dnsmasq-full=y
 CONFIG_PACKAGE_ipv6helper=y
-CONFIG_PACKAGE_ip_full=y
+CONFIG_PACKAGE_ip-full=y
 CONFIG_PACKAGE_ipset=y
 CONFIG_PACKAGE_iptables-mod-tproxy=y
 CONFIG_PACKAGE_resolveip=y
-CONFIG_PACKAGE_ip-full=y
 EOF
