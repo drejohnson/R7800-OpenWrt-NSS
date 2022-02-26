86ec21610a00d314fcbc497a8b23c413163681c1
DeAndre Johnson <dre@phreshr.com>
2022-02-26 23:03:43 +0800

    update customize.sh

--- customize.sh
+++ customize.sh
@@ -3,10 +3,10 @@
 # sed -i 's/192.168.1.1/10.0.0.1/g' openwrt/package/base-files/files/bin/config_generate
 
 # Custom Dependencies:
-cat >> .config <<EOF
-CONFIG_PACKAGE_dnsmasq-full=y
-CONFIG_PACKAGE_ip-full=y
-CONFIG_PACKAGE_ipset=y
-CONFIG_PACKAGE_iptables-mod-tproxy=y
-CONFIG_PACKAGE_resolveip=y
-EOF
+# cat >> .config <<EOF
+# CONFIG_PACKAGE_dnsmasq-full=y
+# CONFIG_PACKAGE_ip-full=y
+# CONFIG_PACKAGE_ipset=y
+# CONFIG_PACKAGE_iptables-mod-tproxy=y
+# CONFIG_PACKAGE_resolveip=y
+# EOF
