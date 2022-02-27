1eeccb93a634d8bc640425d6264de9209b195bad
DeAndre Johnson <dre@phreshr.com>
2022-02-27 13:59:33 +0800

    update

--- customize.sh
+++ customize.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 git clone --depth 1 https://github.com/yichya/luci-app-xray package/luci-app-xray
